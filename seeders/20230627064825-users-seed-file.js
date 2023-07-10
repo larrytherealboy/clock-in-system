7bb98dd3c6c9478045fbd26a5d610b0a3002787a
larrytherealboy <abclarryabclarry@gmail.com>
2023-07-10 14:22:19 +0800

    update: user seeder

--- seeders/20230627064825-users-seed-file.js
+++ seeders/20230627064825-users-seed-file.js
@@ -5,24 +5,17 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
-      account: 'root@example.com',
-      password: await bcrypt.hash('12345678', 10),
+      account: 'root',
+      password: await bcrypt.hash('acuser', 10),
       is_admin: true,
       name: 'root',
       created_at: new Date(),
       updated_at: new Date()
     }, {
-      account: 'user1@example.com',
-      password: await bcrypt.hash('12345678', 10),
+      account: 'user1',
+      password: await bcrypt.hash('acuser', 10),
       is_admin: false,
       name: 'user1',
-      created_at: new Date(),
-      updated_at: new Date()
-    }, {
-      account: 'user2@example.com',
-      password: await bcrypt.hash('12345678', 10),
-      is_admin: false,
-      name: 'user2',
       created_at: new Date(),
       updated_at: new Date()
     }], {})
